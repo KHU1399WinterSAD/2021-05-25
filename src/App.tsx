1bfaeabf1d46b0424a16c7eaccdd236c3704560d
BijanEisapour <bijanprogrammer@gmail.com>
2021-04-07 12:59:31 +0430

    [ADD] shop layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './App.module.scss';
 
 function App() {
-    const name = 'Reza';
+    const [count, setCount] = useState(1);
+    
+    const addToCount = (value: number) => {
+        setCount(count + value);
+    };
     
     return (
         <div className={styles['app']}>
-            <div className={styles['main-container']}>
-                <h1>Hello, {name}!</h1>
-            </div>
+            <header>
+                <a href="#" className={styles['logo']}>Kharazmi Shop Center</a>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Products</a>
+                        </li>
+                    </ul>
+                </nav>
+                <a href="https://github.com/KHU1399WinterSAD" className={styles['github']} target="_blank">GitHub</a>
+            </header>
+            
+            <main>
+                <h1>Shop</h1>
+            </main>
+            
+            <footer>
+                2021 &copy; All rights are reserved
+            </footer>
         </div>
     );
 }
